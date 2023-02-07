5c8b2392aed1f8bf95f0fd856ea6209836c703f9
christopher-t-smith <christophert.smith2016@gmail.com>
2023-02-06 18:08:04 -0600

    Incorporated Mongoose and Post Schema

---
+++ models/postSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  media: {
+    image: { type: String },
+    gifSearch: { type: String }
+  },
+  body: { type: String},
+  tags: { type: String, required: true },
+});
+
+// Require either body or media
+PostSchema.path('body').validate(function(body) {
+    return this.body || this.media.image || this.media.gifSearch;
+  }, 'Body or Media is required');
+
+// Export model
+module.exports = mongoose.model("Post", PostSchema);
