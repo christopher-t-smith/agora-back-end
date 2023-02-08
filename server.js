5cc205f5938bf70ee17fc34c8a49ba69b5e96055
christopher-t-smith <christophert.smith2016@gmail.com>
2023-02-08 15:10:27 -0600

    removed media image data type and iimplemented gif search in backend

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-const getGifs = require("./api_calls/giphy").getGifs;
+const { getGifs } = require("./api_calls/giphy");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Post = require("./models/postSchema");
-const multer = require("multer");
-const fs = require("fs");
-const { promisify } = require("util");
-const readFileAsync = promisify(fs.readFile);
-const Grid = require("gridfs-stream");
 require("dotenv").config();
-
-let gfs;
 
 const app = express();
 
@@ -28,24 +21,7 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(mongoDB);
-
-  mongoose.connection.once("open", () => {
-    gfs = Grid(mongoose.connection.db, mongoose.mongo);
-    gfs.collection("posts");
-  });
 }
-
-// Configure storage mechanism with multer for uploading files set file name format.
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-
-const upload = multer({ storage });
 
 // Giphy API Endpoint
 app.post("/api/giphy", async (req, res) => {
@@ -55,7 +31,7 @@ app.post("/api/giphy", async (req, res) => {
 });
 
 // Post Route
-app.post("/api/posts", upload.single("image"), async (req, res) => {
+app.post("/api/posts", async (req, res) => {
   console.log(req.body);
   if (!req.body.title || !req.body.body || !req.body.tags) {
     return res
@@ -63,49 +39,13 @@ app.post("/api/posts", upload.single("image"), async (req, res) => {
       .json({ message: "Bad request. Required properties are missing." });
   }
 
-  // Create a write stream to store the image file.
-  if (req.file) {
-    const writestream = gfs.createWriteStream({
-      filename: req.file.filename,
-      metadata: {
-        originalname: req.file.originalname,
-        mimetype: req.file.mimetype,
-      },
-    });
-    fs.createReadStream(req.file.path).pipe(writestream);
-    writestream.on("finish", () => {
-      console.log("Image successfully stored in the database");
-    });
-  } else if (req.body.image) {
-    const writestream = gfs.createWriteStream({
-      filename: req.body.image.filename,
-      metadata: {
-        originalname: req.body.image.originalname,
-        mimetype: req.body.image.mimetype,
-      },
-    });
-    fs.createReadStream(req.body.image.path).pipe(writestream);
-    writestream.on("finish", () => {
-      console.log("Image successfully stored in the database");
-    });
-  }  
-
   // Save post to MongoDB database.
   const newPost = new Post({
     username: req.body.username,
     email: req.body.email,
     title: req.body.title,
     media: {
-      image: {
-        filename: req.file ? req.file.filename : "",
-        metadata: req.file
-          ? {
-              originalname: req.file.originalname,
-              mimetype: req.file.mimetype,
-            }
-          : {},
-      },
-      gifSearch: req.body.gifSearch || "",
+      gifSearch: req.body.media.gifSearch || "",
     },
     body: req.body.body,
     tags: req.body.tags,
@@ -127,18 +67,8 @@ app.post("/api/posts", upload.single("image"), async (req, res) => {
 
 // Read Route
 app.get("/api/posts", async (req, res) => {
-  // Decode Base64 string back to binary data and convert it back to an image format.
   try {
     const retrievedPosts = await Post.find();
-    const posts = await Promise.all(
-      retrievedPosts.map(async (post) => {
-        if (req.body.image) {
-          req.body.image = new Buffer.from(req.body.image, "base64");
-        }
-        return post;
-      })
-    );
-
     res.status(200).json({
       message: "Posts retrieved successfully",
       posts: retrievedPosts,
